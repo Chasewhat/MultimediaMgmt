d2ee67cb75503aaf03cba65c16de0389ba4ff58b
Administrator <Administrator@USER-20170721FF>
2018-05-10 17:51:18 +0800

    iccard

--- MultimediaMgmt.View/Controls/ucIcCardMaintance.xaml.cs
+++ MultimediaMgmt.View/Controls/ucIcCardMaintance.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using DevExpress.Mvvm.POCO;
 using DevExpress.Xpf.Core;
 using DevExpress.Xpf.Grid;
+using MultimediaMgmt.View.PopWindows;
 using MultimediaMgmt.ViewModel.Controls;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -20,10 +22,44 @@ namespace MultimediaMgmt.View.Controls
         {
             InitializeComponent();
             this.DataContext = icCardMaintanceViewModel = ViewModelSource.Create<IcCardMaintanceViewModel>();
+            icCardMaintanceViewModel.FileSave = () => { return FileSave(); };
+            icCardMaintanceViewModel.FileOpen = (s) => { FileOpen(s); };
             icCardMaintanceViewModel.MessageShow = (s) =>
             {
                 DevExpress.Xpf.Core.DXMessageBox.Show(s, "提示", MessageBoxButton.OK, MessageBoxImage.Error);
             };
+            icCardMaintanceViewModel.GetExport = (type) =>
+            {
+                MemoryStream ms = new MemoryStream();
+                if (type == 1)
+                    gridView.ExportToXlsx(ms);
+                else
+                    gridView.ExportToXls(ms);
+                return ms;
+            };
+        }
+
+        public string FileSave()
+        {
+            System.Windows.Forms.SaveFileDialog dialog = new System.Windows.Forms.SaveFileDialog();
+            dialog.Filter = "Excel 文件(*.xls)|*.xls|Excel 文件(*.xlsx)|*.xlsx|所有文件(*.*)|*.*";
+            dialog.FilterIndex = 1;
+            if (dialog.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return null;
+            return dialog.FileName;
+        }
+
+        public void FileOpen(string info)
+        {
+            wndFileChoose wfc = new wndFileChoose(info);
+            bool? rs = wfc.ShowDialog();
+            if (rs.HasValue && rs.Value)
+            {
+                bool isOverride = false;
+                string file = string.Empty;
+                wfc.GetResult(ref file, ref isOverride);
+                icCardMaintanceViewModel.ImportExcel(file, isOverride);
+            }
         }
 
         private void Add_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
--- MultimediaMgmt.ViewModel/Controls/IcCardMaintanceViewModel.cs
+++ MultimediaMgmt.ViewModel/Controls/IcCardMaintanceViewModel.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 using MultimediaMgmt.Common.Extend;
 using MultimediaMgmt.Model.Models;
 using MultimediaMgmt.Model;
+using System.Transactions;
+using NPOI.SS.UserModel;
+using System.Reflection;
+using System.Collections;
+using NPOI.HSSF.UserModel;
+using System.IO;
+using NPOI.XSSF.UserModel;
 
 namespace MultimediaMgmt.ViewModel.Controls
 {
@@ -28,9 +35,15 @@ namespace MultimediaMgmt.ViewModel.Controls
 
         public virtual string PersonId { get; set; }
         public virtual string PersonName { get; set; }
+        public virtual string WaitIndiContent { get; set; }
+        public virtual bool IsLoad { get; set; }
+        public Func<string> FileSave;
+        public Action<string> FileOpen;
         public Action<string> MessageShow;
+        public Func<int,MemoryStream> GetExport;
         public IcCardMaintanceViewModel()
         {
+            IsLoad = false;
             Sexs = Constants.Sexs;
             CardTypes = Constants.CardTypes;
             CardStatuss = Constants.CardStatuss;
@@ -58,15 +71,15 @@ namespace MultimediaMgmt.ViewModel.Controls
             if (!string.IsNullOrEmpty(PersonId))
                 data = data.Where(s => s.PersonId == PersonId);
             if (!string.IsNullOrEmpty(HexCode))
-                data = data.Where(s => s.HexCode==HexCode);
+                data = data.Where(s => s.HexCode == HexCode);
             if (!string.IsNullOrEmpty(CardNum))
-                data = data.Where(s => s.CardNum==CardNum);
+                data = data.Where(s => s.CardNum == CardNum);
             if (!string.IsNullOrEmpty(PersonName))
-                data = data.Where(s => s.Name==PersonName);
+                data = data.Where(s => s.Name == PersonName);
             if (!string.IsNullOrEmpty(SelectedCardStatus))
                 data = data.Where(s => s.Status == SelectedCardStatus);
             if (!string.IsNullOrEmpty(SelectedCardType))
-                data = data.Where(s => s.CardType== SelectedCardType);
+                data = data.Where(s => s.CardType == SelectedCardType);
             if (!string.IsNullOrEmpty(SelectedSex))
                 data = data.Where(s => s.Sex == SelectedSex);
             IcCards = data.ToSmartObservableCollection();
@@ -105,6 +118,218 @@ namespace MultimediaMgmt.ViewModel.Controls
 
             multimediaEntities.SaveChanges();
             Query();
+        }
+
+        [Command]
+        public void ImportExcel()
+        {
+            FileOpen("");
+        }
+        public void ImportExcel(string file, bool isOverride)
+        {
+            WaitIndiContent = "正在导入...";
+            IsLoad = true;
+            try
+            {
+                string result = string.Empty;
+                bool task = ImportFromExcel(isOverride, file, ref result);
+                //task.Wait();
+                IsLoad = false;
+                if (!task)
+                {
+                    MessageShow(result);
+                }
+            }
+            catch (Exception e)
+            {
+                IsLoad = false;
+                MessageShow(e.Message);
+            }
+            Query();
+        }
+        [Command]
+        public void ExportExcel()
+        {
+            WaitIndiContent = "正在导出...";
+            IsLoad = true;
+            try
+            {
+                string filePath = FileSave();
+                bool task = ExportToExcel(filePath);
+                //task.Wait();
+                IsLoad = false;
+                if (!task)
+                {
+                    MessageShow("导出时发生异常");
+                }
+            }
+            catch (Exception e)
+            {
+                IsLoad = false;
+                MessageShow(e.Message);
+            }
+        }
+
+        private List<string> IcCardField = new List<string>() { "HexCode", "CardNum", "PersonId", "CardType", "Status" };
+        private List<string> PersonField = new List<string>() { "PersonId", "Name", "Account", "Password", "Sex", "FacultyId", "ClassId", "Email", "Phone", "EntryTime", "DepartureDate" };
+        private List<string> StudentField = new List<string>() { "PersonId", "Name", "Account", "Password", "Sex", "ClassId", "Email", "Phone", "EntryTime" };
+
+        public bool ImportFromExcel(bool overwrite, string file, ref string result)
+        {
+            //return new Task<bool>(() =>
+            //{
+            try
+            {
+                using (TransactionScope ts = new TransactionScope(TransactionScopeOption.RequiresNew,
+                               new System.TimeSpan(1, 30, 0)))
+                {
+                    try
+                    {
+                        if (overwrite)
+                        {
+                            multimediaEntities.Database.ExecuteSqlCommand("delete from [IcCard]");
+                            multimediaEntities.Database.ExecuteSqlCommand("delete from [Person]");
+                            multimediaEntities.Database.ExecuteSqlCommand("delete from [Student]");
+                        }
+                        IWorkbook workbook = WorkbookFactory.Create(file);
+                        IEnumerator rows;
+                        PropertyInfo[] pis = typeof(IcCard).GetProperties();
+                        string sheetName = "IC卡号表";
+                        ISheet sheet = workbook.GetSheet(sheetName);
+                        if (sheet != null)
+                        {
+                            rows = sheet.GetRowEnumerator();
+                            rows.MoveNext();
+                            while (rows.MoveNext())
+                            {
+                                HSSFRow row = (HSSFRow)rows.Current;
+                                IcCard card = new IcCard();
+                                for (int i = 0; i < row.LastCellNum; i++)
+                                {
+                                    PropertyInfo pi = pis.Where(p => p.Name.Equals(IcCardField[i])).FirstOrDefault();
+                                    if (null != pi)
+                                    {
+                                        ICell cell = row.GetCell(i);
+                                        if (cell != null)
+                                        {
+                                            string s = cell.ToString();
+                                            pi.SetValue(card, Convert.ChangeType(cell.ToString(), pi.PropertyType), null);
+                                        }
+                                    }
+                                }
+                                multimediaEntities.IcCard.Add(card);
+                            }
+                        }
+                        pis = typeof(Person).GetProperties();
+                        sheetName = "老师名单";
+                        sheet = workbook.GetSheet(sheetName);
+                        if (sheet != null)
+                        {
+                            rows = sheet.GetRowEnumerator();
+                            rows.MoveNext();
+                            while (rows.MoveNext())
+                            {
+                                HSSFRow row = (HSSFRow)rows.Current;
+                                Person person = new Person();
+                                for (int i = 0; i < row.LastCellNum; i++)
+                                {
+                                    PropertyInfo pi = pis.Where(p => p.Name.Equals(IcCardField[i])).FirstOrDefault();
+                                    if (null != pi)
+                                    {
+                                        ICell cell = row.GetCell(i);
+                                        if (cell != null)
+                                        {
+                                            string s = cell.ToString();
+                                            pi.SetValue(person, Convert.ChangeType(cell.ToString(), pi.PropertyType), null);
+                                        }
+                                    }
+                                }
+                                multimediaEntities.Person.Add(person);
+                            }
+                        }
+                        pis = typeof(Student).GetProperties();
+                        sheetName = "学生名单";
+                        sheet = workbook.GetSheet(sheetName);
+                        if (sheet != null)
+                        {
+                            rows = sheet.GetRowEnumerator();
+                            rows.MoveNext();
+                            while (rows.MoveNext())
+                            {
+                                HSSFRow row = (HSSFRow)rows.Current;
+                                Student student = new Student();
+                                for (int i = 0; i < row.LastCellNum; i++)
+                                {
+                                    PropertyInfo pi = pis.Where(p => p.Name.Equals(IcCardField[i])).FirstOrDefault();
+                                    if (null != pi)
+                                    {
+                                        ICell cell = row.GetCell(i);
+                                        if (cell != null)
+                                        {
+                                            string s = cell.ToString();
+                                            pi.SetValue(student, Convert.ChangeType(cell.ToString(), pi.PropertyType), null);
+                                        }
+                                    }
+                                }
+                                multimediaEntities.Student.Add(student);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        result = ex.Message;
+                        return false;
+                    }
+                    ts.Complete();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message;
+                return false;
+            }
+            //});
+        }
+
+        public bool ExportToExcel(string filename)
+        {
+            
+            FileStream stream = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.Write);
+            try
+            {
+                MemoryStream mstream;
+                IWorkbook workbook;
+                if (Path.GetExtension(filename).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
+                {
+                    mstream = GetExport(1);
+                    if (mstream == null)
+                        return false;
+                    mstream.WriteTo(stream);
+                    workbook = new XSSFWorkbook();
+                    mstream.Close();
+                }
+                else
+                {
+                    mstream = GetExport(0);
+                    if (mstream == null)
+                        return false;
+                    mstream.WriteTo(stream);
+                    workbook = new HSSFWorkbook();
+                    mstream.Close();
+                }
+                workbook.Write(stream);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                stream.Close();
+            }
+            //});
         }
     }
 }
